ff94356e6abdb38f09bceb32f7e08fa887008400
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-03-03 12:53:17 +0000

    generator for F11 organized

--- optimal_packing/Generator.py
+++ optimal_packing/Generator.py
@@ -23,36 +23,58 @@ h_spacing = h_space / (c_full_col + 1)
 #last_line = 3969.0758412570467
 #print(last_line/DELTA)
 
-MAX_CIRCLES = 11443
+MAX_CIRCLES = 11444
 CIRCLE_TYPES = 100
 
 i = 0
 row = 0
 c_type = 0
-c_counter = 1
+c_counter = 0
+color_counter = 0
 
+##
+##  43x 115 Kreise
+##  57x 114 Kreise
+##
+circle_types = []
+for c in range(8):
+    for i in range(CIRCLE_TYPES):
+        if i % 8 == c:
+            circle_types.append(i)
+
+# easy way: i % 100
+# clustering way:
 def circle_type(i: int) -> int:
     global c_counter
     global c_type
-    c_max = 114 if c_type < 56 else 115
+    c_max = 115 if c_type < 44 else 114
     if c_counter == c_max:
-        c_counter = 1
+        c_counter = 0
         c_type += 1
 
     c_counter += 1
-    return c_type - 1
+    return circle_types[c_type]
 
-y = c_radius + h_spacing
+double_line = 57
+
+y = c_radius
 while y < 4000:
-    delta = 0 if row % 2 == 0 else c_radius
-    for c in range(1, int(c_full_line - (row % 2)), 2):
+    delta = 0 if row % 2 == 0 or row == double_line else c_radius
+    if row == double_line:
+        c_in_line = int(c_full_line)
+    else:
+        c_in_line = int(c_full_line - (row % 2))
+    for c in range(1, c_in_line, 2):
         print(f'{delta + c*c_radius} {y} 20 {circle_type(i)}')
         i += 1
     row += 1
-    y += DELTA + h_spacing
+    if row == double_line or row == (double_line + 1):
+        y += (c_radius * 2)
+    else:
+        y += DELTA
 
 row -= 1
-y -= (DELTA + h_spacing)
+y -= (DELTA)
 
 show_stats = False
 if show_stats:
