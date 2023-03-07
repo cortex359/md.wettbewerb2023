8dca935ab9f6274e186a972fb6efb3bf09a94524
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-03-07 10:04:21 +0000

    shell scripts moved

--- scripts/results.zsh
+++ scripts/results.zsh
@@ -5,7 +5,7 @@ projectPath="/${(j./.)${(s./.)PWD}[1,${${(s./.)PWD}[(I)md.wettbewerb2023]}]}"
 function check_files() {
   if (( ARGC < 2 )) {
     echo "${0} [01..14] [resultFile1] [resultFile2] [...]"
-    exit 1
+    return -1
   }
   i=$1
   for a in ${argv[2,-1]} ; do
@@ -13,10 +13,10 @@ function check_files() {
   done
 }
 
-function compaire() {
+function compare_results() {
   if (( ARGC < 2 )) {
     echo "${0} [01..14] [resultFile1] [resultFile2] [...]"
-    exit 1
+    return -1
   }
   i=$1
   python ${projectPath}/scoring/score.py ${projectPath}/input/forest${i}.txt \
